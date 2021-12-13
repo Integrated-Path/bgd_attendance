749b0414422c716541e709fb7edbe39e8e837d1c
M-Saeb <mohammed.saeb98@hotmail.com>
2021-12-13 12:24:27 +0300

    finished task & did code review

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "bgd_attendance",
+    'name': "Baghdad Hotel Attandance",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """This report is to print a custom attandace report for Baghdad Hotel""",
 
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "INTEGRATED PATH",
+    'author': "Integerated Path",
     'website': "https://www.int-path.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Attendances',
+    'version': '13.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr_attendance'],
@@ -29,6 +23,4 @@
         'report/attendance_report.xml',
         
     ],
-    # only loaded in demonstration mode
-    'demo': [],
 }
